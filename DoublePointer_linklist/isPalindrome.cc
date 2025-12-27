08a798cf80fee9924edf62e42a8b0e933b658ff9
natsumehe <qiuluo1001001@gmail.com>
2025-12-27 13:23:05 +0800

    234题 判断回文链表 将链表输入数组中双指针判断  后半段链表反转后判断

--- DoublePointer_linklist/isPalindrome.cc
+++ DoublePointer_linklist/isPalindrome.cc
@@ -2,6 +2,29 @@
 #include "../util/print_util.hpp"
 
 class Solution{
+    private:
+        SingleListNode<int>::ListNode* reverseList(SingleListNode<int>::ListNode* head){
+            SingleListNode<int>::ListNode* pre = nullptr;
+            SingleListNode<int>::ListNode* cur = head;
+            while(cur != nullptr){
+                SingleListNode<int>::ListNode* next = cur->next;
+                cur->next = pre;
+                pre = cur;
+                cur = next;
+            }
+            return pre;
+        }
+        
+        SingleListNode<int>::ListNode* endoffHalf(SingleListNode<int>::ListNode* head){
+            SingleListNode<int>::ListNode* fast = head;
+            SingleListNode<int>::ListNode* slow = head;
+            while(fast != nullptr && fast ->next != nullptr){
+                fast = fast->next->next;
+                slow = slow ->next;
+            }
+            return slow;
+        }
+
     public:
     /// @brief 复制到数组然后用双指针
     /// @param head 
@@ -20,6 +43,27 @@ class Solution{
         }
         return true;
     }
+
+    /// @brief 后半段反过来的方法
+    /// @param head 
+    /// @return result
+    bool reverse_doublepointer(SingleListNode<int>::ListNode* head){
+        SingleListNode<int>::ListNode* firsthalf = endoffHalf(head);
+        SingleListNode<int>::ListNode* secondhalf = reverseList(firsthalf-> next);
+
+        auto p1 = head,p2 = secondhalf;
+        bool result = true;
+        while(result && p2){
+            if(p1->val != p2-> val){
+                return false;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        firsthalf ->next = reverseList(secondhalf);
+        return result;
+    }
+
 };
 
 int main() {
@@ -30,7 +74,7 @@ int main() {
     SingleListNode<int>::ListNode* Node1 = SingleListNode<int>::vectorToListNode(vec1);
     
     bool result = sol.reversecheck(Node);
-    bool result2 = sol.reversecheck(Node1);
+    bool result2 = sol.reverse_doublepointer(Node1);
     std::cout<< "输出的值："<<result<<endl;
     std::cout<< "不是回文数的判断："<<result2<<endl;
     return 0;
